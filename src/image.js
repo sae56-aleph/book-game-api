bcf891960ebc9185334a613687e6a8bcb75ff04f
Enzo MAROS <enzo.maros@gmail.com>
2024-04-04 18:31:36 +0200

    Refactorisation API Image

--- src/image.js
+++ src/image.js
@@ -1,40 +1,29 @@
-import('node-fetch').then(fetch => {
-    async function main() {
-      const payload = JSON.stringify({
-        prompt: "Alice au pays des merveilles. Vous êtes Alice. Vous vous allongez dans l’herbe et contemplez le ciel. Les étoiles scintillent intensément, formant des constellations étranges. Vous reconnaissez Orion, mais il a une jambe de plus. La Grande Ourse ressemble à un chat endormi. Soudain, une étoile filante traverse le ciel. Que souhaitez-vous ?",
+async function fetchImage(prompt) {
+    const payload = JSON.stringify({
+        prompt: `Alice au pays des merveilles. Vous êtes Alice. ${prompt}`,
         aspect_ratio: "1:1",
         quality: "LOW",
         style: "PHOTOREALISTIC",
-      });
-  
-      const host = "https://api.limewire.com";
-      const pathname = "/api/image/generation";
-      const requestOptions = {
-        method: 'POST',
+    });
+
+    const host = "https://api.limewire.com";
+    const pathname = "/api/image/generation";
+    const requestOptions = {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'X-Api-Version': 'v1',
-          'Accept': 'application/json',
-          'Authorization': 'Bearer <ici>'
+            "Content-Type": "application/json",
+            "X-Api-Version": "v1",
+            Accept: "application/json",
+            Authorization: `Bearer ${process.env.LIMEWIRE_API}`,
         },
-        body: payload
-      };
-  
-      const response = await fetch.default(host + pathname, requestOptions);
-      
-      if (response.status === 200) {
-        const responseData = await response.json();
-        if (responseData.data.length > 0) {
-          return responseData.data[0].asset_url;
-        } else {
-          throw new Error('La réponse ne contient pas de données d\'actif');
-        }
-      } else {
-        return null;
-      }
-    }
-  
-    main().then(assetUrl => {
-      console.log(assetUrl);
-    }).catch(error => console.error(error));
-  });
+        body: payload,
+    };
+
+    const response = await fetch(host + pathname, requestOptions);
+    const responseData = await response.json();
+
+    if (response.status != 200) return null;
+    if (responseData.data.length == 0) return null;
+
+    return responseData.data[0].asset_url;
+}
