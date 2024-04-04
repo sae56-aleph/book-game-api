dfb9dda0dd25e3d8086317780012aef6d19e42b7
SimonnCode <simonfouchet1712@gmail.com>
2024-04-04 10:59:44 +0200

    return url

--- src/image.js
+++ src/image.js
@@ -1,8 +1,10 @@
 import('node-fetch').then(fetch => {
     async function main() {
       const payload = JSON.stringify({
-        prompt: "A cute baby sea otter",
-        aspect_ratio: "1:1"
+        prompt: "Alice au pays des merveilles. Vous êtes Alice. Vous vous allongez dans l’herbe et contemplez le ciel. Les étoiles scintillent intensément, formant des constellations étranges. Vous reconnaissez Orion, mais il a une jambe de plus. La Grande Ourse ressemble à un chat endormi. Soudain, une étoile filante traverse le ciel. Que souhaitez-vous ?",
+        aspect_ratio: "1:1",
+        quality: "LOW",
+        style: "PHOTOREALISTIC",
       });
   
       const host = "https://api.limewire.com";
@@ -20,19 +22,19 @@ import('node-fetch').then(fetch => {
   
       const response = await fetch.default(host + pathname, requestOptions);
       
-      if (!response.ok) {
-        throw new Error('La requête a échoué avec le statut ' + response.status);
+      if (response.status === 200) {
+        const responseData = await response.json();
+        if (responseData.data.length > 0) {
+          return responseData.data[0].asset_url;
+        } else {
+          throw new Error('La réponse ne contient pas de données d\'actif');
+        }
+      } else {
+        return null;
       }
-  
-      const responseData = await response.json();
-  
-      if (Object.keys(responseData).length === 0) {
-        throw new Error('La réponse JSON est vide');
-      }
-  
-      console.log(responseData);
     }
   
-    main().catch(error => console.error(error));
+    main().then(assetUrl => {
+      console.log(assetUrl);
+    }).catch(error => console.error(error));
   });
-  
