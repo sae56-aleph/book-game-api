94ab6eecf04216048d7a031ebbc6d538be5ec75d
Anthonin Vincentelli <anthoninvincentelli@gmail.com>
2024-04-05 08:47:00 +0200

    Création script de démarrage de l'application avec peuplement de la base de données

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+directory="."
+file=".db_filled"
+
+# Check if the file exists
+if [ ! -f "$directory/$file" ]; then
+    # If the file does not exist, run a few commands
+    echo "Populating database. Running commands..."
+    # Commands
+    sleep 5
+    npx prisma migrate dev
+    npx prisma db seed
+    touch .db_filled
+
+fi
+
+npm run start
